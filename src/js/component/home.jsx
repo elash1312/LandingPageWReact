37dc8f4c2770f7b53695bb830cbf2faa68e244f7
elash1312 <lasherin1312@gmail.com>
2022-04-22 17:44:33 +0000

    Navbar added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,18 +6,61 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#">
+						Start Bootstrap
+					</a>
+					<button
+						class="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNav"
+						aria-controls="navbarNav"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div
+						class="collapse navbar-collapse justify-content-end"
+						id="navbarNav">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a
+									class="nav-link active"
+									aria-current="page"
+									href="#">
+									Home
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">
+									About
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">
+									Services
+								</a>
+							</li>
+							<li class="nav-item">
+								<a
+									class="nav-link disabled"
+									href="#"
+									tabindex="-1"
+									aria-disabled="true">
+									Contact
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
 			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
+				Made by Erin Lash @{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+				❤️!
 			</p>
 		</div>
 	);
