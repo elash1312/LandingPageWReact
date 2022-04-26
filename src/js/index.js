188e6df3ea265da82f9178cb89e1c6cae86b12df
elash1312 <lasherin1312@gmail.com>
2022-04-26 21:00:12 +0000

    added jumbotron component

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,13 @@ import "../styles/index.css";
 //import your own components
 // import Home from "./component/home.jsx";
 import Navbar from "./component/navbar.jsx";
+import Jumbotron from "./component/jumbotron.jsx";
 
 //render your react application
 // ReactDOM.render(<Home />, document.querySelector("#app"));
-ReactDOM.render(<Navbar />, document.querySelector("#app"));
+ReactDOM.render(
+	<div>
+		<Navbar />, <Jumbotron />
+	</div>,
+	document.querySelector("#app")
+);
